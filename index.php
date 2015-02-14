f56b1226e824943cc12df09c1300fe07319b4a9c
mayrtondias <mayrtondias@hotmail.com>
2015-02-14 19:45:23 -0300

    identação

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
-	<head>
-		<title>Amanda!!!!</title>
-	</head>
-	<body>
-		<? echo "<h1>Que os jogos comecem!!!!!! </h1>";?>
-	</body>
+    <head>
+        <title>Amanda!!!!</title>
+    </head>
+    <body>
+        <? echo "<h1>Que os jogos comecem!!!!!! </h1>";?>
+    </body>
 
-<html>
+    <html>
